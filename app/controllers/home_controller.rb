6e8d6a516c8d9251edc693cc82c2ce96667f133f
Margus Ernits <margus.ernits@itcollege.ee>
2010-09-03 16:21:29 +0300

    index page - tasklist beeta

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @week_n=params[:n2dal] || @week_n='53'
-    @robot = Robot.find(:all)
+    @week_n=params[:n2dal] || 'week'+@week_n=Time.now.strftime('%W')
+    @robots = Robot.find(:all)
+    #@tasks=Task.find(:conditions => ["week_no == ?", 35 ], :include => :robot:all)
     @tasks=Task.find(:all)
     @all_weeks=State.find( :all, :select => ' DISTINCT week_no' )
+    @states=State.find(:all, :conditions => ["week_no==?",35])
   end
   
   def week
