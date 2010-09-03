543c16fac74c0da401dffacc8a2d602c234bb7eb
tiiatanav <tiiatanav@hotmail.com>
2010-09-03 16:48:27 +0300

    statistika esilehel, nädalate valik

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @week_n=params[:n2dal] || 'week'+@week_n=Time.now.strftime('%W')
+    @week_n=params[:n2dal][:week_no] || @week_n=Time.now.strftime('%W')
     @robots = Robot.find(:all)
     #@tasks=Task.find(:conditions => ["week_no == ?", 35 ], :include => :robot:all)
     @tasks=Task.find(:all)
     @all_weeks=State.find( :all, :select => ' DISTINCT week_no' )
-    @states=State.find(:all, :conditions => ["week_no==?",35])
+    @states=State.find(:all, :conditions => ["week_no==?",@week_n])
   end
   
   def week
