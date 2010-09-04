d9a62f161cf2b4aa113a4758f9e75e11fa216ae9
Margus Ernits <margus.ernits@gmail.com>
2010-09-04 19:39:30 +0300

    parandatud robotite urlid http->https

--- db/migrate/20100901112912_robot_initial_data.rb
+++ db/migrate/20100901112912_robot_initial_data.rb
@@ -1,13 +1,13 @@
 class RobotInitialData < ActiveRecord::Migration
   def self.up
     Robot.create(:name =>'madistajad', 
-    :homepage =>'http://robot.itcollege.ee:3000/projekt/projects/madistajad', 
+    :homepage =>'https://robot.itcollege.ee:3000/projekt/projects/madistajad', 
     :content =>'siia tuleb midagi')
      Robot.create(:name =>'tont', 
-    :homepage =>'http://robot.itcollege.ee/wiki/index.php/Nimetu', 
+    :homepage =>'https://robot.itcollege.ee/wiki/index.php/Nimetu', 
     :content =>'siia tuleb midagi')
       Robot.create(:name =>'digipallur', 
-    :homepage =>'http://robot.itcollege.ee/wiki/index.php/Digipallur', 
+    :homepage =>'https://robot.itcollege.ee/wiki/index.php/Digipallur', 
     :content =>'siia tuleb midagi')
      
   end
