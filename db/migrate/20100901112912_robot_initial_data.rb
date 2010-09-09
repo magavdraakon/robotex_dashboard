a842a70bfcbd15811a856951c52079ebce1d99f0
Margus Ernits <margus.ernits@itcollege.ee>
2010-09-09 14:48:05 +0300

    uus robot, esilehele 4 robotit

--- db/migrate/20100901112912_robot_initial_data.rb
+++ db/migrate/20100901112912_robot_initial_data.rb
@@ -1,4 +1,5 @@
 class RobotInitialData < ActiveRecord::Migration
+  
   def self.up
     Robot.create(:name =>'madistajad', 
     :homepage =>'https://robot.itcollege.ee:3000/projekt/projects/madistajad', 
@@ -9,7 +10,9 @@ class RobotInitialData < ActiveRecord::Migration
       Robot.create(:name =>'digipallur', 
     :homepage =>'https://robot.itcollege.ee/wiki/index.php/Digipallur', 
     :content =>'siia tuleb midagi')
-     
+     Robot.create(:name =>'AaaEee', 
+    :homepage =>'https://robot.itcollege.ee/wiki/', 
+    :content =>'siia tuleb midagi')     
   end
 
   def self.down
